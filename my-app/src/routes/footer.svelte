9286ab8db3a7ada944fa3752f89ed2a405eb86c0
Poetic-Terrorism <koutann020@gmail.com>
2024-12-08 16:18:17 +0900

    good change

---
+++ my-app/src/routes/footer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import * as config from '$lib/config'
+        import { CornerDownRightIcon } from 'lucide-svelte'
+    </script>
+
+    <footer>
+<p><a href="https://x.com/jplwvp">{config.title}</a>&copy {new Date().getFullYear()} </p>
+    </footer>
+
+    <style>
+        footer{
+            padding-block: var(--size-7);
+            border-top: 1px solid var(--border);
+
+        }
+
+        p{
+
+            color: var(--text-2)
+        }
+
+
+    </style>
